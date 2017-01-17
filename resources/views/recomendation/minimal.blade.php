653b38d9a8f3c238bf421130848aba88cb9c2ed9
Amrani Houssain <amranidev@gmail.com>
2017-01-17 21:29:59 +0000

    update minimal

--- resources/views/recomendation/minimal.blade.php
+++ resources/views/recomendation/minimal.blade.php
@@ -4,12 +4,9 @@
 		<meta charset="UTF-8">
 		<title>Recomendation system</title>
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-		<!-- Latest compiled and minified JavaScript -->
-		<link href="http://visjs.org/dist/vis-network.min.css" rel="stylesheet" type="text/css" />
-		<script type="text/javascript" src="http://visjs.org/dist/vis.js"></script>
+		<link rel="stylesheet" href="{{asset('css/app.css')}}">
+		<link rel="stylesheet" href="{{asset('css/vis.css')}}">
+		<script type="text/javascript" src="{{asset('js/vis.js')}}"></script>
 		<script type="text/javascript">
 		var DIR = 'img/soft-scraps-icons/';
 		var nodes = null;
